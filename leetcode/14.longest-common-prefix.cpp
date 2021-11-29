0e4275b88c9609217211c0bcee5e8c0ca6beb48b
raj patel <kakhilesh79@gmail.com>
2021-11-29 13:20:11 +0530

    sum problem done

--- leetcode/14.longest-common-prefix.cpp
+++ leetcode/14.longest-common-prefix.cpp
@@ -5,20 +5,12 @@
  */
 
 // @lc code=start
-class Solution {
+class Solution
+{
 public:
-    string longestCommonPrefix(vector<string>& strs) {
-        int a = 0;
-        string pref = strs[0];
-        
-        for (int i = 1; i <= strs.size() - 1; i++){
-
-            pref = commonPrefixUtil(pref, strs[i]);
-        }
-
-        return pref;
+    string longestCommonPrefix(vector<string> &strs)
+    {
        
     }
 };
 // @lc code=end
-
